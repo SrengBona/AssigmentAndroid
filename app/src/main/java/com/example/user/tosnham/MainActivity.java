6e2a8dead2f4a8638eb08b92290634621403a55b
Bona <srengbona@gmail.com>
2018-06-30 21:35:19 +0700

    Create Drawerlayout in Main Activity.(30/06/2018)

--- app/src/main/java/com/example/user/tosnham/MainActivity.java
+++ app/src/main/java/com/example/user/tosnham/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.user.tosnham;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar tlbMain = findViewById(R.id.tlb_main);
+        setSupportActionBar(tlbMain);
+        getSupportActionBar().setTitle("Tos Nham");
+
+        // Create actionbar drawer toggle
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, tlbMain, R.string.app_name, R.string.app_name);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
     }
 }
