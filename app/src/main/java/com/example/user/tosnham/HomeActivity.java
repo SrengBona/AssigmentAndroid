81fd55f237ac02800377dc7d2a1c182a475ff235
Dell <choemkosal4@gmail.com>
2019-03-01 08:28:09 +0700

    yy

--- app/src/main/java/com/example/user/tosnham/HomeActivity.java
+++ app/src/main/java/com/example/user/tosnham/HomeActivity.java
@@ -2,6 +2,10 @@ package com.example.user.tosnham;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.facebook.accountkit.AccountKit;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //Sign out user
+        Button btnSignOut = (Button) findViewById(R.id.btnLogOut);
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AccountKit.logOut();
+                finish();
+            }
+        });
     }
 }
